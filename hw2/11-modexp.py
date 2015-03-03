9b387e26dd2019b39b17ffc6e96456b320fd28e8
Anu <anu.varshneya@gmail.com>
2015-03-03 00:40:41 -0800

    final edits - ready to turn in!

--- hw2/11-modexp.py
+++ hw2/11-modexp.py
@@ -1,12 +1,22 @@
-def modexp(a, b, c, p):
-	if(b==1): return a%p
-	x = modexp(a,b>>1,p)
-	x = (x*x)%p
-	if(b&1==1)==1: # for odd number
-        x = (x*a)%p
-return x
+def mod_exp(a, b, p):
+    if (b == 1): 
+    	return a % p
+    x = modexp(a, b>>1, p)
+    x = (x * x) % p
+    if (b & 1 == 1): 
+    	x = (x * a) % p
+    return x
 
 def exp(b, c):
-	if (c==1): return b
-	x = 
+    if (c == 1): 
+    	return b
+    z = exp(b, c>>1)
+    z = z * z
+    if (c & 1 == 1): 
+    	z = z * b
+    return z
 
+def complex_mod_exp(a, b, c, p):
+    pow1 = exp(b, c)
+    res = modexp(a, pow1, p)
+    return res
