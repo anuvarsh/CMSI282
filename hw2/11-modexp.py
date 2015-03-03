6ec1543f79cd6d452bb342e00e60f17c6572f0b6
Anu <anu.varshneya@gmail.com>
2015-03-03 10:02:34 -0800

    fixed some typos

--- hw2/11-modexp.py
+++ hw2/11-modexp.py
@@ -1,7 +1,7 @@
 def mod_exp(a, b, p):
     if (b == 1): 
     	return a % p
-    x = modexp(a, b>>1, p)
+    x = mod_exp(a, b>>1, p)
     x = (x * x) % p
     if (b & 1 == 1): 
     	x = (x * a) % p
@@ -18,5 +18,5 @@ def exp(b, c):
 
 def complex_mod_exp(a, b, c, p):
     pow1 = exp(b, c)
-    res = modexp(a, pow1, p)
+    res = mod_exp(a, pow1, p)
     return res
