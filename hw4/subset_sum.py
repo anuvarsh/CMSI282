3436e9d05d85751480156d11ebcc6dea6b249970
Anu <anu.varshneya@gmail.com>
2015-05-04 22:07:47 -0700

    Kirkman added, and done!

--- hw4/subset_sum.py
+++ hw4/subset_sum.py
@@ -1,27 +1,6 @@
-
-x = [1,4,6,8,5]
-target_sum = 16
-
-#A combines all positive values in x, and B combines all negative values
-A = 0
-B = 0
-for i in range(len(x)):
-	if (x[i] < 0):
-		A += x[i]
-	else:
-		B += x[i]
-
-N = len(x);
-
-#initialize intermediate results array with initial values
-prevQvalues = {}
-for j in range(A,B+1):
-	prevQvalues[(0,j)] = (x[0] == j)
-
-
 #this computes values of "is there a subset of length i, which sums to s"
 #Q(i,s) := Q(i-1,s) || (x[i] == s) || Q(i-1,s-x[i])
-def Q(i,s):
+def Q(i,s,A,B,prevQvalues):
 	if ((s<A) or (s>B)):
 		return False
 
@@ -32,11 +11,33 @@ def Q(i,s):
 
 	return res
 
-for i in range(1,N):
-	for j in range(A,B+1):
-		prevQvalues[(i,j)] = Q(i,j)
+def app(x, target_sum):
+	A = 0
+	B = 0
 
-print "Given list = ", x, ",  Target sum = ", target_sum
-print "... and the result is"
-print Q(N-1,target_sum)
+	for i in range(len(x)):
+		if (x[i] < 0):
+			A += x[i]
+		else:
+			B += x[i]
+
+	N = len(x);
+
+	prevQvalues = {}
+	for j in range(A, B+1):
+		prevQvalues[(0,j)] = (x[0] == j)
+
+	for i in range(1,N):
+		for j in range(A, B+1):
+			prevQvalues[(i,j)] = Q(i,j,A,B,prevQvalues)
+
+	return Q(N-1,target_sum,A,B,prevQvalues)
+
+
+if __name__ == '__main__':	
+	x = [1,4,6,8,5]
+	target_sum = 16
+	print "Given list = ", x, ",  Target sum = ", target_sum
+	print "the result is..."
+	print app(x, target_sum)
 
